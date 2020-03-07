769b55aa57b58fefd2abe8df01596755460c21e1
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-03-07 23:33:12 +0000

    Insert right commit! (#2)

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - function that inserts a node as the right-child.
+ * @parent: This is the parent
+ * @value: This is the payload
+ * Return: A pointer to the new node or NULL if failure.
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+binary_tree_t *node;
+
+	if (parent == NULL)
+		return (NULL);
+
+	node = malloc(sizeof(binary_tree_t));
+	if (node == NULL)
+		return (NULL);
+
+	node->n = value;
+	node->parent = parent;
+	node->left = NULL;
+	node->right = NULL;
+
+	if (parent->right != NULL)
+	{
+		node->right = parent->right;
+		parent->right->parent = node;
+	}
+	parent->right = node;
+
+	return (node);
+}
