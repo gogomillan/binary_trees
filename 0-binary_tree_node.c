dc4b3f0b2bc69c5a78e7e026113184f70beee68b
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-03-07 22:16:45 +0000

    Fix lack of comments in New node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,15 +1,19 @@
 #include "binary_trees.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * binary_tree_node - function that creates a binary tree node
+ * @parent: This is the parent
+ * @value: This is the payload
+ * Return: A pointer to the new node or NULL if failure.
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 binary_tree_t *node;
 
 	node = malloc(sizeof(binary_tree_t));
+	if (node == NULL)
+		return (NULL);
+
 	node->n = value;
 	node->left = NULL;
 	node->right = NULL;
