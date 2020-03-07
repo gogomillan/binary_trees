27981303cef63f3151a61e2d53db8ebd08126ecc
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-03-07 22:09:47 +0000

    New node commit! (#0)

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+binary_tree_t *node;
+
+	node = malloc(sizeof(binary_tree_t));
+	node->n = value;
+    node->left = NULL;
+    node->right = NULL;
+
+	if (parent == NULL)
+		node->parent = NULL;
+	else
+		node->parent = parent;
+
+    return (node);
+}
