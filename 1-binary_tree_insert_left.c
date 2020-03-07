f0656b53235e105b3a1d7f700c1a54868295df06
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-03-07 22:41:31 +0000

    Insert left commit! (#1)

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - function that inserts a node as the left-child.
+ * @parent: This is the parent
+ * @value: This is the payload
+ * Return: A pointer to the new node or NULL if failure.
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+binary_tree_t *node;
+
+	if (parent == NULL)
+		return (NULL);
+
+	node = malloc(sizeof(binary_tree_t));
+	if (node == NULL)
+		return (NULL);
+
+	node->n = value;
+	node->left = NULL;
+	node->right = NULL;
+
+	if (parent->left != NULL)
+		node->left = parent->left;
+	parent->left = node;
+
+	return (node);
+}
