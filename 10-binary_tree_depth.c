d97210379e68ae192576cf67afcc3f81d6eb307b
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-03-08 02:17:28 +0000

    Depth commit! (#10)

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - measures the depth of a node in a binary tree.
+ *
+ * @tree: Pointer to the node
+ * Return: The depth of the node
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (tree == NULL || tree->parent == NULL)
+		return (0);
+
+	return (binary_tree_depth(tree->parent) + 1);
+}
