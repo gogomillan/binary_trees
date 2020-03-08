0b5171bb60ec2f42c2b0d4431fa371d9702e0a93
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-03-08 01:58:56 +0000

    Height commit! (#9)

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - function that measures the height of a binary tree.
+ *
+ * @tree: Pointer to the node
+ * Return: The height of the tree starting at @node
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+size_t hl; /* height of left subtree */
+size_t hr; /* height of right subtree */
+
+	if (tree->left != NULL)
+		hl = 1 + binary_tree_height(tree->left);
+	else
+		hl = 0;
+
+	if (tree->right != NULL)
+		hr = 1 + binary_tree_height(tree->right);
+	else
+		hr = 0;
+
+	if (hl > hr)
+		return (hl);
+	else
+		return (hr);
+}
