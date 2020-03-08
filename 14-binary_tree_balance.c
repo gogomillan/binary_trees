fe776d4907e3167fb117e32ff0877fd34358eb7a
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-03-08 03:46:54 +0000

    Balance factor commit! (#14)

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,43 @@
+#include "binary_trees.h"
+
+int get_max(int left, int right);
+int get_height(const binary_tree_t *tree);
+
+/**
+* binary_tree_balance - measures the balance factor of a binary tree.
+* @tree: the node of the tree to measure the balance factor of
+* Return: balance factor or 0
+**/
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (get_height(tree->left) - get_height(tree->right));
+}
+
+/**
+* get_height - calculates height of root
+* @tree: pointer to root node of tree to count height of
+* Return: height or 0
+**/
+int get_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (1 + get_max(get_height(tree->left), get_height(tree->right)));
+}
+
+/**
+* get_max - returns max between two numbers
+* @left: number 1
+* @right: number 2
+* Return: the maximum
+**/
+int get_max(int left, int right)
+{
+	if (left > right)
+		return (left);
+	return (right);
+}
