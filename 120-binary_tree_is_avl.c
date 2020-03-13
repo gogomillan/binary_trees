bb66700fc92b5f58b669fbfaac561775eac03ecd
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-03-13 22:50:40 +0000

    Is AVL, commit! (#30)

---
+++ 120-binary_tree_is_avl.c
@@ -0,0 +1,76 @@
+#include "binary_trees.h"
+#include <limits.h>
+
+/**
+* greater - finds the maximum number between two
+* @left: fisrt number
+* @right: second number
+* Return: max of two numbers
+**/
+int greater(size_t left, size_t right)
+{
+	if (left >= right)
+		return (left);
+	return (right);
+}
+
+/**
+* find_height - finds the height of a binary tree
+* @tree: pointer to the root node of the tree to check
+* Return: height of tree
+**/
+int find_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+
+	return (1 + greater(find_height(tree->left),
+						find_height(tree->right)));
+}
+
+/**
+* is_balanced - checks if a given bst is balanced or not
+* @tree: pointer to the root node of the tree to check
+* Return: 1 if balanced, 0 otherwise
+**/
+int is_balanced(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (1);
+
+	if (abs(find_height(tree->left) - find_height(tree->right)) > 1)
+		return (0);
+	return (is_balanced(tree->left) && is_balanced(tree->right));
+}
+
+/**
+* is_bst - check if a given tree is valid binary search tree or not
+* @tree: pointer root node of the tree to check
+* @min: lower limit of node value
+* @max: upper limit of node value
+* Return: 1 if bst, 0 otherwise
+**/
+int is_bst(const binary_tree_t *tree, int min, int max)
+{
+	if (tree == NULL)
+		return (1);
+	if (tree->n < min || tree->n > max)
+		return (0);
+	return (is_bst(tree->left, min, tree->n - 1) &&
+		is_bst(tree->right, tree->n + 1, max));
+}
+
+/**
+* binary_tree_is_avl - checks if a binary tree is a valid AVL Tree
+* @tree: pointer to the root node of the tree to check
+* Return: 1 if tree is a valid AVL Tree, and 0 if not or tree is NULL
+**/
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+	if (tree != NULL && is_bst(tree, INT_MIN, INT_MAX) && is_balanced(tree))
+		return (1);
+	return (0);
+}
